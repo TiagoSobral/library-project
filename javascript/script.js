3832ebef9b5a613c959fe9506f1084624acd0478
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2025-05-23 17:50:00 +0100

    (feat) func to display books in a table. -> forEach loop created to iterate trough array.     -> create element tr and td to display as a     column.

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,25 @@
-const myLibrary = ["Harry Potter: The Philosopher Stone", "Harry Potter: The Goblet of Fire", "Hunger Games"];
+const myLibrary = [
+    "Harry Potter: The Philosopher Stone", 
+    "Harry Potter: The Goblet of Fire", 
+    "Hunger Games"];
 
-function Book(title, author) {
-    this.title = title;
-    this.author = author;
+function Book() {
 };
 
 function addBookToLibrary() {};
+
+const tableBody = document.querySelector("tbody");
+
+function displayOnPage(array) {
+    array.forEach( (element) => {
+        let bookRow = document.createElement("tr");
+        let bookInfo = document.createElement("td");
+        bookInfo.textContent = element;
+        tableBody.appendChild(bookRow);
+        bookRow.appendChild(bookInfo);
+    });
+};
+
+displayOnPage(myLibrary);
 
 
