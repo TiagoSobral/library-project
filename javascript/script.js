f55572e56d765f44197d28874cffb003ce38df0d
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2025-05-23 16:55:25 +0100

    (feat) add library array & main functions

---
+++ javascript/script.js
@@ -0,0 +1,10 @@
+const myLibrary = ["Harry Potter: The Philosopher Stone", "Harry Potter: The Goblet of Fire", "Hunger Games"];
+
+function Book(title, author) {
+    this.title = title;
+    this.author = author;
+};
+
+function addBookToLibrary() {};
+
+
